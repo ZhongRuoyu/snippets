8ea7af7291c84117a02fb990ae847cab9f52d0a7
Ruoyu Zhong <zhongruoyu@outlook.com>
2026-05-03 13:27:35 +0800

    Remove some snippets

--- algorithms/quine/quine.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-    char str[]="#include <stdio.h>%c%cint main(void)%c{%c%cchar str[]=%c%s%c;%c%cprintf(str,10,10,10,10,9,34,str,34,10,9,10,9,10);%c%creturn 0;%c}";
-    printf(str,10,10,10,10,9,34,str,34,10,9,10,9,10);
-    return 0;
-}
